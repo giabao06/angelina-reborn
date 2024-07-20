3ccda89d134baeaf7045bd8970b566f0e5ccba07
Nguyễn Phạm Gia Bảo <giabao.nguyenpham@e.email>
2024-07-20 22:51:59 +0700

    initial work bringing back prefixes because slash cmds suck...

---
+++ src/commands/hello.js
@@ -0,0 +1,12 @@
+const Command = require ("../structs/Command.js");
+const {MessageEmbed} = require('discord.js');
+
+module.exports = new Command({
+    name: "hello",
+    alias: [],
+    description: "hello... world? or planet? who knows",
+
+    async run(message,args,client){
+        message.channel.send("hello! if you see this, the prefix ver. of `angelina-reborn` is working!");
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,58 +1,44 @@
 const { Client, Events, GatewayIntentBits, Collection } = require ('discord.js');
 const { token, prefix } = require ('../data/config.json');
+const Command = require("./structs/Command.js");
 
+class Client_ext extends Client{
+    constructor(options){
+        super({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] })
+
+        /**
+         * @type {Discord.Collection<string, Command>}
+         */
+        this.commands =  new Collection();
+    }
+}
+
+const client = new Client_ext();
 
 const fs = require('node:fs');
 const path = require('node:path');
 
+fs.readdirSync('./commands')
+    .filter(file => file.endsWith(".js"))
+    .forEach(file => {
+        /**
+		 * @type {Command}
+		 */
+        const cmd = require(`./commands/${file}`);
+        client.commands.set(Command.name,cmd);
+        console.log(`loaded command ${Command.name}`);
+    });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});
-
-client.commands = new Collection();
-
-const cmdDir = path.join(__dirname, 'commands');
-const cmdFolders= fs.readdirSync(cmdDir);
-
-for (const folder of cmdFolders){
-    const cmdPath = path.join(cmdDir, folder);
-    const cmdFiles = fs.readdirSync(cmdPath).filter(file => file.endsWith('.js'));
-    for (const file of cmdFiles) {
-        const fPath = path.join(cmdPath, file);
-        const cmd = require (fPath);
-        if ('data' in cmd && 'execute' in cmd) {
-            client.commands.set(cmd.data.name, cmd);
-            console.log(`loaded command ${cmd.data.name}`);
-        } else {
-            console.log (`${fPath} is incomplete, maybe. fix it ya dummy`);
-        }
-    }
-}
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const cmd=interaction.client.commands.get(interaction.commandName);
-    
-    if (!cmd) {
-        console.error(`${interaction.commandName} isnt here yet`);
-        return;
-    }
-
-    try {
-        await cmd.execute(interaction);
-    }
-    catch (error){
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({content: 'a bad happend :(', ephemeral: true});
-        } else {
-            await interaction.reply({content: 'a bad happend :(', ephemeral: true});
-        }
-    }
-    console.log(`${interaction.user.globalName} (@${interaction.user.username}) used ${interaction.commandName}`);
-});
-
+client.on("messageCreate", message =>{
+    if (message.author.bot) return;
+    if(message.content.slice(0, prefix.length).toLowerCase() !== prefix) return;
+    const args = message.content.substring(prefix.length).split(/ +/);
+    var command = client.commands.find(cmd => cmd.name == args[0]);
+    if (!command) var command =  client.commands.find(cmd => cmd.alias == args[0]);
+    if (!command) return message.reply(`invalid command: ${args[0]}`)
+})
 client.once(Events.ClientReady, readyClient => {
-    client.user.setPresence({ activities: [{ name: 'in a sandbox (?%)' }], status: 'idle' });
+    client.user.setPresence({ activities: [{ name: 'in a sandbox (?%, prefix ver.)' }], status: 'idle' });
     console.log(`logon success, user=${readyClient.user.tag}`);
 });
 
