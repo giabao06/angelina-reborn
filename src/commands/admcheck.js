3f336f1315e51eee9d223464a130a596d48ac89f
Nguyễn Phạm Gia Bảo <giabao.nguyenpham@e.email>
2024-07-21 22:55:47 +0700

    ... well I forgot about null results

--- src/commands/admcheck.js
+++ src/commands/admcheck.js
@@ -8,6 +8,7 @@ module.exports = new Command({
     async run(message, args, client){
         const dbP = await db;
         const row = await dbP.get(`SELECT roles from adminRoles where guild_id = '${message.guildId}'`);
+        if (row == null) {return message.channel.send("guild not in database!")}
         const admRoles=JSON.parse(row.roles);
         if (row.roles == "") {return message.channel.send("no admin roles configured for guild!")}
         if (message.member.roles.cache.some(role => admRoles.includes(String(role.id)))) {message.reply("true");}
--- src/utils/admcheck.js
+++ src/utils/admcheck.js
@@ -7,6 +7,7 @@ const Discord = require ("discord.js");
 module.exports = async function isAdmin(message){
     const dbP = await db;
     const row = await dbP.get(`SELECT roles from adminRoles where guild_id = '${message.guildId}'`);
+    if (row == null) {return false;}
     const admRoles=JSON.parse(row.roles);
     if (row.roles == "") {return false;}
     if (message.member.roles.cache.some(role => admRoles.includes(String(role.id)))) {return true;}
